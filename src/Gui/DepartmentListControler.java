5c684556af258bc3b6f5e4c52b7b590094cdaea7
vinicius96lima <viniciusaraujo_96@hotmail.com>
2023-04-24 14:05:17 -0300

    DepartmentForm (dialog) design

--- src/Gui/DepartmentListControler.java
+++ src/Gui/DepartmentListControler.java
@@ -1,18 +1,24 @@
 package Gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import Gui.util.Utils;
 import application.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -38,8 +44,9 @@ public class DepartmentListControler implements Initializable {
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("Funcionou");
+	public void onBtNewAction(javafx.event.ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	@Override
@@ -68,6 +75,24 @@ public class DepartmentListControler implements Initializable {
 		List <Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+	
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department Data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);//nao deixa acessar a janela anterior sem fecha-la.
+			dialogStage.showAndWait();
+	}
+		catch (IOException e) {
+			e.getMessage();
+		}
 	}
 
 }
